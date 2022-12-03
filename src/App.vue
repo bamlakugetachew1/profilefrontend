3ed10c8e8c02ef986a26983c21396be174ebc1b8
bamlakugetachew1 <101706961+bamlakugetachew1@users.noreply.github.com>
2022-12-03 10:53:45 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,7 @@ export default {
         var fileURL = window.URL.createObjectURL(new Blob([response.data]));
         var fURL = document.createElement("a");
         fURL.href = fileURL;
-        fURL.setAttribute("download", "bamlaku_getachew_cv.jpg");
+        fURL.setAttribute("download", "bamlaku_getachew_cv.pdf");
         document.body.appendChild(fURL);
         fURL.click();
       });
