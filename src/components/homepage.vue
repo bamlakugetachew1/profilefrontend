db7a5962a0f7fb31addc20db8ff03a98433fd0c0
bamlakugetachew1 <101706961+bamlakugetachew1@users.noreply.github.com>
2022-12-12 16:17:21 +0300

    Update homepage.vue

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -109,6 +109,7 @@ export default {
     </head>
 
     <body :class="this.display == 1 ? ' bg-gray-200' : 'bg-zinc-500'">
+     
       <!--  -->
       <div class="pt-8 md:px-20 lg:px-40">
         <nav class="flex justify-between px-5">
@@ -215,6 +216,7 @@ export default {
         <h3 class="text-center pb-7 text-2xl text-gray-800 pt-5 font-serif">
           Availlable Groups
         </h3>
+       <p v-if="this.availlablegroups.length == 0" class="capitalize text-center  mx-14 mt-3 mb-2" >not found avalliable groups please creat one</p> 
 
         <div class="md:flex flex-wrap justify-between gap-3">
           <div
